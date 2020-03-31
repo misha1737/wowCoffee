c013190c0b49d104e867a8ba7c231ce708141b29
misha1737 <mikekozlov2@gmail.com>
2020-03-31 13:09:49 +0300

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,36 @@
-console.log('secondFile Ok');
+var images = [
+  "./assets/wowCoffe-1.png",
+  "./assets/wowCoffe-2.png",
+  "./assets/wowCoffe-3.png"
+];
+var next = document.querySelector("#next");
+var previous = document.querySelector("#previous");
+var slider = document.querySelector("#slides");
+var curentSlide = 0;
+
+function addImage() {
+  for (i in images) {
+    slide = document.createElement("img");
+    slide.src = images[i];
+    document.querySelector("#slides").appendChild(slide);
+  }
+}
+function curentImage(i) {
+  var slides = slider.getElementsByClassName("active");
+  if (slides.length > 0) {
+    slides[0].className = "";
+  }
+  slider.getElementsByTagName("img")[i].className = "active";
+}
+
+addImage();
+curentImage(curentSlide);
+
+next.addEventListener("click", function() {
+  curentSlide == images.length - 1 ? (curentSlide = 0) : curentSlide++;
+  curentImage(curentSlide);
+});
+previous.addEventListener("click", function() {
+  curentSlide == 0 ? (curentSlide = images.length - 1) : curentSlide--;
+  curentImage(curentSlide);
+});
